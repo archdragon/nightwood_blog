b6557d0ca15df1074c6e7ac758ad8707e6917d11
Przemyslaw Krowinski <hello@krowinski.com>
2018-11-23 21:12:32 +0100

    Draft blogposts and better mbobile styles

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,20 @@
 //= require_tree .
+var eye0 = document.querySelector(".eye1");
+var eye1 = document.querySelector(".eye2");
+
+document.addEventListener("mousemove", function(e) {
+  var box0 = eye0.getBoundingClientRect();
+  var box1 = eye1.getBoundingClientRect();
+
+  var x0 = box0.left + box0.width/2 - e.clientX;
+  var y0 = box0.top + box0.width/2 - e.clientY;
+
+  var x1 = box1.left + box1.width/2 - e.clientX;
+  var y1 = box1.top + box1.width/2 - e.clientY;
+
+  var angle0 = Math.atan2(0 - x0, y0) * (180/Math.PI);
+  var angle1 = Math.atan2(0 - x1, y1) * (180/Math.PI);
+
+  eye0.style.transform = "rotate(" + angle0 + "deg)";
+  eye1.style.transform = "rotate(" + angle1 + "deg)";
+});
